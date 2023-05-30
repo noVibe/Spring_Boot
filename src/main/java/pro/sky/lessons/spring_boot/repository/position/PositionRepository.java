45b2ce9c4cc279a09bff39e86834831c6ce08461
noVibe <117359029+noVibe@users.noreply.github.com>
2023-05-30 16:16:38 +0300

    Revert "DTO pattern added"

--- src/main/java/pro/sky/lessons/spring_boot/repository/position/PositionRepository.java
+++
@@ -1,7 +0,0 @@
-package pro.sky.lessons.spring_boot.repository.position;
-
-import org.springframework.data.repository.CrudRepository;
-import pro.sky.lessons.spring_boot.model.Position;
-
-public interface PositionRepository extends CrudRepository<Position, Long> {
-}
--- src/main/java/pro/sky/lessons/spring_boot/repository/report/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package pro.sky.lessons.spring_boot.repository.report;
-
-import org.springframework.data.repository.CrudRepository;
-import pro.sky.lessons.spring_boot.model.Report;
-
-public interface ReportRepository extends CrudRepository<Report, Long> {
-}
